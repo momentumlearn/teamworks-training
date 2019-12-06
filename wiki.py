d0be87d6dafeeded4fd3aaa5dca3dfba86d88558
Clinton Dreisbach <clinton@dreisbach.us>
2019-12-06 11:29:59 -0500

    Add skeleton and tests for check_wiki_word

--- wiki.py
+++ wiki.py
@@ -1,4 +1,17 @@
+import string
+
+
 def check_wiki_link(candidate):
+    """
+    Given a string, detect if it is a valid wiki link. A wiki link should
+    start with two square brackets, have either text for the label and
+    target or have the label and target separated by a vertical pipe, and
+    end with two square brackets. Examples:
+
+    * [[Link text]]
+    * [[Link label|Link target]]
+    """
+
     if not (candidate[:2] == "[[" and candidate[-2:] == "]]"):
         return None
 
@@ -13,3 +26,9 @@ def check_wiki_link(candidate):
         return None
 
     return {"label": label, "target": target}
+
+
+def check_wiki_word(candidate):
+    valid_characters = string.ascii_letters + string.digits
+
+    # TODO
--- wiki_test.py
+++ wiki_test.py
@@ -1,4 +1,4 @@
-from wiki import check_wiki_link
+from wiki import check_wiki_link, check_wiki_word
 
 
 def test_bad_links():
@@ -16,3 +16,23 @@ def test_bad_links():
 def test_good_links():
     assert check_wiki_link("[[Hello]]") == {"label": "Hello", "target": "Hello"}
     assert check_wiki_link("[[Hello|Hi]]") == {"label": "Hello", "target": "Hi"}
+
+
+def test_bad_wiki_words():
+    assert check_wiki_word("") is None
+    assert check_wiki_word("Test") is None
+    assert check_wiki_word("testWord") is None
+    assert check_wiki_word("Test Word") is None
+    assert check_wiki_word("1TestWord") is None
+    assert check_wiki_word("Test-Word") is None
+
+
+def test_good_wiki_words():
+    assert check_wiki_word("TestWord") == {
+        "label": "TestWord",
+        "target": "TestWord"
+    }
+    assert check_wiki_word("TestWord1") == {
+        "label": "TestWord1",
+        "target": "TestWord1"
+    }
