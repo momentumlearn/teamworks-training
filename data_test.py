b58533ba3f71739eae328459280519bbffc446ed
Clinton Dreisbach <clinton@dreisbach.us>
2019-12-07 16:40:13 -0500

    Fix data tests

--- data_test.py
+++ data_test.py
@@ -1,5 +1,6 @@
 from data import DBObject
 import pytest
+import sqlite3
 
 
 class Widget(DBObject):
@@ -26,16 +27,17 @@ class Widget(DBObject):
 
         return "INSERT INTO WIDGETS (name) VALUES (?)", [self.name]
 
-    def validate(self):
+    def validate(self, db=None):
         if not self.name:
             return False
         return True
 
 
-@pytest.fixture(autouse=True)
+@pytest.fixture
 def db():
-    DBObject.set_database(":memory:")
-    Widget.create_table()
+    db = sqlite3.connect(":memory:")
+    Widget.create_table(db)
+    return db
 
 
 def test_creating_widget():
@@ -44,32 +46,32 @@ def test_creating_widget():
     assert widget.name == "Test"
 
 
-def test_saving_widget_to_db():
+def test_saving_widget_to_db(db):
     widget = Widget(name="Test")
-    widget.save()
+    widget.save(db)
     assert widget.id is not None
 
 
-def test_invalid_widget_cannot_be_saved():
+def test_invalid_widget_cannot_be_saved(db):
     widget = Widget()
-    was_saved = widget.save()
+    was_saved = widget.save(db)
     assert not was_saved
     assert widget.id is None
 
 
-def test_retrieving_widget_from_db():
+def test_retrieving_widget_from_db(db):
     widget = Widget(name="Test")
-    widget.save()
+    widget.save(db)
 
-    widgets = Widget.select("WHERE id = ?", [widget.id])
+    widgets = Widget.select(db, "WHERE id = ?", [widget.id])
     assert widgets[0].id == widget.id
     assert widgets[0].name == widget.name
 
 
-def test_deleting_widget_from_db():
+def test_deleting_widget_from_db(db):
     widget = Widget(name="Test")
-    widget.save()
-    widget.delete()
+    widget.save(db)
+    widget.delete(db)
 
-    widgets = Widget.select("WHERE id = ?", [widget.id])
+    widgets = Widget.select(db, "WHERE id = ?", [widget.id])
     assert widgets == []
