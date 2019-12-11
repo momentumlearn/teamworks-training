7efe43d99b165be1e5440bb165c52cc2a7c81c2f
Clinton Dreisbach <clinton@dreisbach.us>
2019-12-11 11:21:34 -0500

    Add webpack for compiling JavaScript
    * Also add moment to show using a JS dependency

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,3 +1,5 @@
+const moment = require('moment')
+
 const pages = [
   {
     body: "*Python* is an interpreted, high-level, general-purpose programming language. Created by Guido van Rossum and first released in 1991, Python's design philosophy emphasizes code readability with its notable use of significant whitespace. Its language constructs and object-oriented approach aim to help programmers write clear, logical code for small and large-scale projects.\n\nPython is [[dynamically typed|Dynamic programming language]] and garbage-collected. It supports multiple programming paradigms, including procedural, object-oriented, and functional programming. Python is often described as a \"batteries included\" language due to its comprehensive standard library.\n\nPython was conceived in the late 1980s as a successor to the ABC language. Python 2.0, released in 2000, introduced features like list comprehensions and a garbage collection system capable of collecting reference cycles. Python 3.0, released in 2008, was a major revision of the language that is not completely backward-compatible, and much Python 2 code does not run unmodified on Python 3.",
@@ -47,11 +49,17 @@ function displayWikiTitles () {
   const list = document.createElement('ul')
   for (const page of pages) {
     const listItem = document.createElement('li')
+    const strongEl = document.createElement('strong')
     const text = document.createTextNode(page.title)
+    const updatedAt = moment(page.updated_at).fromNow()
+    const updatedText = document.createTextNode(updatedAt)
     listItem.addEventListener('click', function (event) {
       toggleWikiBody(listItem, page.body)
     })
-    listItem.appendChild(text)
+    strongEl.appendChild(text)
+    listItem.appendChild(strongEl)
+    listItem.appendChild(document.createTextNode(' '))
+    listItem.appendChild(updatedText)
     list.appendChild(listItem)
   }
 
