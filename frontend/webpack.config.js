dee4845cfb30aaca7039297f6c69a373714ab70f
Clinton Dreisbach <clinton@dreisbach.us>
2019-12-11 11:29:16 -0500

    Add babel for compiling modern JS to ES5

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,5 +1,17 @@
 module.exports = {
   entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js']
+  },
   output: {
     path: __dirname + '/dist',
     publicPath: '/',
